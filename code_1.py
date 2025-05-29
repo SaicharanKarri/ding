71189ca77f41308a94df5576be0dbb4abf242629
Karri Sai Charan <saicharank1397@gmail.com>
2025-05-29 15:46:33 +0530

    first commit

---
+++ code_1.py
@@ -0,0 +1,15 @@
+from abc import ABC,abstractmethod
+
+class project(ABC):
+
+    @abstractmethod
+    def task1(self):
+        pass
+
+    @abstractmethod
+    def task2(self):
+        pass
+    
+    @abstractmethod
+    def task3(self):
+        pass
